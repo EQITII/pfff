c8d7bbb271320c6a5088b3d5ca810a172555884d
Andrew Malyarchuk <andrew_cybervision@eqitii.com>
2018-08-09 17:50:57 +0300

    remote result parsing

--- pfff.py
+++ pfff.py
@@ -3,20 +3,9 @@
 import sys, subprocess
 from functools import reduce
 
-if len(sys.argv) < 2:
-    print(sys.argv[0], '<file...>')
-    sys.exit(2)
+arguments = sys.argv
+arguments.pop(0)
 
-files = sys.argv
-files.pop(0)
-
-result = subprocess.run(['./build/src/pfff', '-B'] + files, stdout=subprocess.PIPE)
+result = subprocess.run(['./build/src/pfff'] + arguments, stdout=subprocess.PIPE)
 resultString = result.stdout.decode('utf-8')
-
-hashes = list(filter(lambda s: len(s), resultString.split('\n')))
-for hash in hashes:
-    print(hash)
-
-if len(hashes) > 1:
-    areEqual = reduce(lambda a, b: a == b, hashes)
-    print('Equal:', areEqual)
+print(resultString)
